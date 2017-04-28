6b1596f5afab5d94ea952c96d864adf4a58bb535
Eugen K <ek926m@gmail.com>
2017-04-28 12:54:16 +0200

    Delete excuses.sh

--- metadata_script/excuses.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-
-import random; 
-
-list = open("excuses.txt", "r")
-excuses = list.readlines()
-excuses = [x.strip() for x in excuses]
-list.close()
-print(random.choice(excuses));

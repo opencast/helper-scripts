74bc708c3b7f5e4cd7c7a06be338d0d22c4d39fd
mheyen <malte.heyen@posteo.de>
2021-02-25 12:27:56 +0100

    updated config file
    - added dictionary for tenant_urls
    - updated documentation

--- multi-tenant-configuration/config.py
+++ multi-tenant-configuration/config.py
@@ -1,16 +1,19 @@
 # Configuration
 
-#Set this to your global admin node
+# Set this to your global admin node
 url = "http://tenant1:8080"
-#If you have multiple tenants use something like
-#url_pattern = "https://{}.example.org"
-#otherwise, url_pattern should be the same as the url variable above
-url_pattern = "http://{}:8080"
-# list of tenant URLs
-tenant_urls = [
-    'http://tenant1:8080',
-    'http://tenant2:8080'
-]
+
+# If you have multiple tenants use an URL pattern:
+# tenant_url_pattern = "https://{}.example.org"
+# ToDo otherwise, this can be empty or commented out
+tenant_url_pattern = "http://{}:8080"
+# ToDo You can also define a dictionary of tenant URLs, which will be prioritized over the URL pattern:
+# example:
+# tenant_urls = { '<tenant1.id>': 'http://tenant1:8080', '<tenant1.id>': 'http://tenant2:8080' }
+tenant_urls = {
+    'tenant1': 'http://tenant1:8080',
+    'tenant2': 'http://tenant2:8080'
+}
 
 # digest user
 digest_user = "opencast_system_account"

4ebb74d913b24051640f681f0e8fa51fe059be31
mheyen <malte.heyen@posteo.de>
2021-06-30 11:36:44 +0200

    Series are now being created and more meta data was added to the events.
    The script can now be configured to create a specified number of Series. The events will be randomly added to certain Series.
    More meta data (location, series, and more acl details) are added to each event.
    Changed the testing video file in the configurations.

--- create_oc_testdata/config.py
+++ create_oc_testdata/config.py
@@ -1,19 +1,19 @@
 # Configuration
 
-# target url for the Opencast system
-# "https://tenant1.opencast.com"
+# default target url for the Opencast system
 target_url = "http://localhost:8080"
 
-# path to a test video
-test_video_path = '/home/malte/IdeaProjects/helper-scripts/create_oc_testdata/video_test.mp4'
+# default path to a test video
+test_video_path = '/home/malte/IdeaProjects/helper-scripts/create_oc_testdata/test_video.mkv'
 
 # default value for the number of events to be created
-number_of_events = 2
+number_of_events = 6
+# default value for the number of series to be created
+number_of_series = 2
+# default value for the number of copies per event to be created
+number_of_copies = 2
 
 # digest login
 digest_user = "opencast_system_account"
 digest_pw = "CHANGE_ME"
 
-# workflow config
-workflow_id = "reimport-workflow"
-workflow_config = {"autopublish": "false"}
--- create_oc_testdata/main.py
+++ create_oc_testdata/main.py
@@ -12,53 +12,81 @@ import string
 import random
 
 
-alphabet = list(string.ascii_letters)
-for i in range(10):
-    alphabet.append(str(i))
-for c in ['Ä','Ö','Ü','ä','ö','ü']:
-    alphabet.append(c)
-
-
 def main():
     """
     Populate the specified Opencast system with random sample events
     """
 
-    # use api/events/{id} ?
-
-    target_url, number_of_events = parse_args()
+    target_url, number_of_events, file_path = parse_args()
     digest_login = DigestLogin(user=config.digest_user, password=config.digest_pw)
 
     if not target_url:
         target_url = config.target_url
     if not number_of_events:
         number_of_events = config.number_of_events
+    if not file_path:
+        file_path = config.test_video_path
 
     print(number_of_events)
     print(target_url)
 
     print("Starting population process.")
 
-    # test request
+    # test API connection
     url = f'{target_url}/api/info/me'
     try:
-        response = get_request(url, digest_login, "events")
+        get_request(url, digest_login, "events")
     except:
         __abort_script("Something went wrong. No Connection to API. Stopping script. ")
 
-    for i in range(number_of_events):
+    # Serien erstellen
+    series = []
+
+    for i in range(config.number_of_series):
+
+        url = f'{target_url}/series/'
+        id = __generate_random_name()
+        series.append(id)
+        data = {
+            'identifier': id,
+            'publisher': __generate_random_name(),
+            'title': __generate_random_name(),
+            'acl': '{"acl": {"ace": ['
+                   '{"allow": true,"role": "ROLE_ANONYMOUS","action": "write"}, '
+                   '{"allow": true,"role": "ROLE_ADMIN","action": "read"}'
+                   ']}}'
+        }
+
         try:
-            # create event
-            url = f'{target_url}/ingest/addMediaPackage/fast'
-            files = [config.test_video_path]
+            response = post_request(url, digest_login, "series", data=data)
+            print(response)
 
-            data = {
-                'creator': __generate_random_name(),
-                'title': __generate_random_name(),
-                'flavor': 'presentation/source',
-                'acl': '{"acl": {"ace": [{"allow": true,"role": "ROLE_Z","action": "dance"}]}}'
-            }
+        except Exception as e:
+            print("ERROR")
+            print(str(e))
+            __abort_script("Something went wrong. Could not create series. Stopping script")
 
+    # Events erstellen
+    for i in range(number_of_events):
+
+        url = f'{target_url}/ingest/addMediaPackage'    # schedule-and-upload
+        files = [file_path]
+
+        data = {
+            'creator': __generate_random_name(),
+            'title': __generate_random_name(),
+            'flavor': 'presentation/source',
+            'description': 'This is a test description. This Event is only for testing purposes. ',
+            'spatial': __generate_random_name(),
+            'isPartOf': random.choice(series),
+            'acl': '{"acl": {"ace": ['
+                   '{"allow": true,"role": "ROLE_ADMIN","action": "read"}, '
+                   '{"allow": true,"role": "ROLE_ADMIN","action": "write"}, '
+                   '{"allow": true,"role": "ROLE_ANONYMOUS","action": "read"}'
+                   ']}}'
+        }
+
+        try:
             response = big_post_request(url, digest_login, "events", data=data, files=files)
             print(response)
 
@@ -67,15 +95,26 @@ def main():
             print(str(e))
             __abort_script("Something went wrong. Could not create event. Stopping script")
 
-        print("Done.")
+        # ToDo copy the Events
+
+    print("Done.")
+
+
+def __create_alphabet():
+    alphabet = list(string.ascii_letters)
+    for i in range(10):
+        alphabet.append(str(i))
+    for c in ['Ä','Ö','Ü','ä','ö','ü']:
+        alphabet.append(c)
+    return alphabet
+
+alphabet = __create_alphabet()
 
 
 def __generate_random_name():
-
     name = ''
     for i in range(random.randint(1, 30)):
         name += random.choice(alphabet)
-
     return name
 
 

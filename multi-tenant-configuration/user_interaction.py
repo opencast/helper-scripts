0e11c1af5dda30858c69d08eb466d641b672f744
mheyen <malte.heyen@posteo.de>
2021-08-30 19:50:47 +0200

    Added doc strings and moved private functions

--- multi-tenant-configuration/user_interaction.py
+++ multi-tenant-configuration/user_interaction.py
@@ -11,6 +11,21 @@ HELP_OPTION = 'h'
 
 
 def check_or_ask_for_permission(target_type, action, target_name, tenant_id, option_i=False) -> bool:
+    """
+    Check if a permission for the action was already given or asks for permission.
+
+    :param target_type: The target for the action (either 'user' or 'group')
+    :type target_type: str
+    :param action: The action to be performed
+    :type action: str
+    :param target_name: The group or user name
+    :type target_name: str
+    :param tenant_id: The target tenant
+    :type tenant_id: str
+    :param option_i: Whether the user has the option to perform the action iteratively.
+    :type option_i: bool
+    :return: bool, whether the action should be performed.
+    """
 
     # check if permission is already defined
     permission = get_permission(target_type, action, target_name, tenant_id)
@@ -24,6 +39,20 @@ def check_or_ask_for_permission(target_type, action, target_name, tenant_id, opt
 
 
 def get_permission(target_type, action, target_name, tenant_id) -> bool:
+    """
+    Returns the permission for the given action.
+    If no permission value is found, None is returned
+
+    :param target_type: The target for the action (either 'user' or 'group')
+    :type target_type: str
+    :param action: The action to be performed
+    :type action: str
+    :param target_name: The group or user name
+    :type target_name: str
+    :param tenant_id: The target tenant
+    :type tenant_id: str
+    :return: bool or None, the permission value
+    """
 
     log('permissions: ', permissions)
 
@@ -44,13 +73,36 @@ def get_permission(target_type, action, target_name, tenant_id) -> bool:
 
 
 def ask_user(target_type, action, target_name, tenant_id, option_i=False) -> str:
+    """
+    Asks the user for permission to perform a certain action.
+    Returns the answer.
+    The answer can be stored if the user specifies if the answer holds for:
+    - all tenants, or
+    - all targets (i.e. all groups or all ), or
+    - both.
+    This can done by adding 'a' or 't' to the answer.
+    For example:
+    'yat' corresponds to 'Yes, always do this action on all tenants for this target'.
 
-    individual_option = "\n    Write 'i' to perform the action individually for each case. " if option_i else ""
+    :param target_type: The target for the action (either 'user' or 'group')
+    :type target_type: str
+    :param action: The action in question
+    :type action: str
+    :param target_name: The group or user name
+    :type target_name: str
+    :param tenant_id: The target tenant
+    :type tenant_id: str
+    :param option_i: Whether the user has the option to answer with i.
+    :type option_i: bool
+    :return: str, the answer of the user
+    """
 
-    help_description = f"""    Write 'y' to perform the action. Write 'n' to skipp this action. {individual_option}
-    Add 't' for 'tenant' or 'a' for 'all' to store your decision for this or all tenants.
-    Add 't' for 'target' or 'a' for 'all' to store your decision for this or all targets.
-    EXAMPLE: Write 'yat' to store your decision for the action on ALL tenants and for THIS target.
+    individual_option = "\nWrite 'i' to perform the action individually for each case. " if option_i else ""
+
+    help_description = f"""Write 'y' to perform the action. Write 'n' to skipp this action. {individual_option}
+Add 't' for 'tenant' or 'a' for 'all' to store your decision for this or all tenants.
+Add 't' for 'target' or 'a' for 'all' to store your decision for this or all targets.
+EXAMPLE: Write 'yat' to store your decision for the action on ALL tenants and for THIS target.
 """
 
     question = f"Do you want to {action} ({target_type} {target_name} on {tenant_id})? Write '{HELP_OPTION}' for help.\n"
@@ -62,26 +114,30 @@ def ask_user(target_type, action, target_name, tenant_id, option_i=False) -> str
         if answer == HELP_OPTION:
             answer = input(help_description)
         # return all valid answers
-        elif parsable(answer) or (option_i and answer == 'i'):
+        elif __parsable(answer, option_i):
             return answer
         # catch all invalid answers
         else:
             answer = input(f"Invalid answer. Write '{HELP_OPTION}' for help.\n").lower()
 
 
-def parsable(answer) -> bool:
-
-    if re.match(ANSWER_PATTERN, answer) or answer == HELP_OPTION:
-        return True
-    else:
-        return False
-
-
-def __build_key(action, tenant, target):
-    return action + ':' + tenant + ':' + target
-
-
 def process_answer(answer, target_type, action, target_name, tenant_id, option_i) -> bool:
+    """
+    Processes an answer and, if specified, stores it as a permission.
+    Returns a boolean, whether the action should be performed.
+
+    :param target_type: The target for the action (either 'user' or 'group')
+    :type target_type: str
+    :param action: The action to be performed
+    :type action: str
+    :param target_name: The group or user name
+    :type target_name: str
+    :param tenant_id: The target tenant
+    :type tenant_id: str
+    :param option_i: Whether the user has the option to perform the action iteratively.
+    :type option_i: bool
+    :return: bool, whether the action should be performed.
+    """
 
     # simple yes or no case (not stored)
     if answer == 'y':
@@ -100,3 +156,24 @@ def process_answer(answer, target_type, action, target_name, tenant_id, option_i
     permissions[target_type][key] = permission_value
 
     return permission_value
+
+
+def __parsable(answer, option_i=False) -> bool:
+    """
+    Checks if an answer is parsable, i.e. matches the answer pattern.
+
+    :param answer: The answer given by the user
+    :type answer: str
+    :param option_i: Whether 'i' is an acceptable answer
+    :type option_i: str
+    :return: bool, whether the answer is parsable
+    """
+
+    return re.match(ANSWER_PATTERN, answer) or answer == HELP_OPTION or (option_i and answer == 'i')
+
+
+def __build_key(action, tenant, target):
+    """
+    Builds the key to store the permission in the dictionary.
+    """
+    return action + ':' + tenant + ':' + target

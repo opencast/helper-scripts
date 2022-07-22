ceeb5d740282f243b8f13fd98d39105c425daa4f
Lars Kiesow <lkiesow@uos.de>
2022-07-22 16:40:54 +0200

    Migrate Opencast Events

---
+++ migrate-events-to-another-opencast/migrate.py
@@ -0,0 +1,143 @@
+#!/usr/bin/env python3
+
+import json
+import urllib3
+
+SOURCE_HOST = 'https://develop.opencast.org'
+SOURCE_USER = 'admin'
+SOURCE_PASS = 'opencast'
+
+TARGET_HOST = 'http://localhost:8080'
+TARGET_USER = 'admin'
+TARGET_PASS = 'opencast'
+TARGET_WORKFLOW = 'import'
+
+
+http = urllib3.PoolManager()
+
+
+def get_published_series():
+    '''
+    Generator, requesting all published media packages from the search service
+    of the source Opencast. The media packages hold links to all published
+    media and can be passed on as-is to the target Opencast.
+
+    :return: Tuple of series Dublin Core XML and ACL XML
+    '''
+    url_search = f'{SOURCE_HOST}/search/series.json'
+    headers = urllib3.make_headers(basic_auth=f'{SOURCE_USER}:{SOURCE_PASS}')
+
+    offset = 0
+    total = 1
+    limit = 2
+
+    # get published media packages from source opencast
+    while total > offset:
+        request = http.request('GET', url_search, headers=headers, fields={
+            'limit': limit,
+            'offset': offset})
+        data = request.data.decode('utf-8')
+        data = json.loads(data).get('search-results')
+
+        offset = data.get('offset') + limit
+        total = data.get('total')
+        results = data.get('result', [])
+        if type(results) is not list:
+            results = [results]
+
+        for result in results:
+            series_id = result.get('id')
+            print('Importing', series_id)
+
+            # get dublin core
+            url_dc = f'{SOURCE_HOST}/series/{series_id}.xml'
+            request = http.request('GET', url_dc, headers=headers)
+            dublincore = request.data.decode('utf-8').strip()
+
+            # get acl
+            url_acl = f'{SOURCE_HOST}/series/{series_id}/acl.xml'
+            request = http.request('GET', url_acl, headers=headers)
+            acl = request.data.decode('utf-8').strip()
+
+            yield (dublincore, acl)
+
+
+def create_series(dublincore, acl):
+    '''
+    Creates a series with given metadata and access control rules in the target
+    Opencast.
+
+    :param dublincore: Dublin Core metadata XML
+    :type dublincore: str
+    :param acl: Access control list as XML
+    :type acl: str
+    '''
+    url_series = f'{TARGET_HOST}/series/'
+    headers = urllib3.make_headers(basic_auth=f'{TARGET_USER}:{TARGET_PASS}')
+
+    request = http.request('POST', url_series, headers=headers, fields={
+        'series': dublincore,
+        'acl': acl})
+    print('Create series response:', request.status)
+
+
+def get_published_media():
+    '''
+    Generator, requesting all published media packages from the search service
+    of the source Opencast. The media packages hold links to all published
+    media and can be passed on as-is to the target Opencast.
+
+    :return: Media package XML
+    '''
+    url_search = f'{SOURCE_HOST}/search/episode.json'
+    headers = urllib3.make_headers(basic_auth=f'{SOURCE_USER}:{SOURCE_PASS}')
+
+    offset = 0
+    total = 1
+    limit = 2
+
+    # get published media packages from source opencast
+    while total > offset:
+        request = http.request('GET', url_search, headers=headers, fields={
+            'limit': limit,
+            'offset': offset})
+        data = request.data.decode('utf-8')
+        data = json.loads(data).get('search-results')
+
+        offset = data.get('offset') + limit
+        total = data.get('total')
+        results = data.get('result', [])
+        if type(results) is not list:
+            results = [results]
+
+        for result in results:
+            print('Importing ' + result.get('id'))
+            yield result.get('ocMediapackage')
+
+
+def ingest(mediapackage):
+    '''
+    Takes a media package XML and ingests it to the target Opencast.
+
+    :param mediapackage: Media package as XML
+    :type mediapackage: str
+    '''
+    url_ingest = f'{TARGET_HOST}/ingest/ingest'
+    headers = urllib3.make_headers(basic_auth=f'{TARGET_USER}:{TARGET_PASS}')
+
+    request = http.request('POST', url_ingest, headers=headers, fields={
+        'mediaPackage': mediapackage,
+        'workflowDefinitionId': TARGET_WORKFLOW})
+    print('Ingest response:', request.status)
+
+
+def main():
+    for dublincore, acl in get_published_series():
+        create_series(dublincore, acl)
+
+    for mediapackage in get_published_media():
+        ingest(mediapackage)
+
+
+if __name__ == '__main__':
+    main()
---
+++ migrate-events-to-another-opencast/readme.md
@@ -0,0 +1,34 @@
+# Migrate Published Recordings to Another Opencast
+
+This script lets you quickly migrate your published series and recordings from one to another Opencast.
+It will not migrate any asset manager content.
+
+For the migration, please ensure that static files are served without authentication and without job context evaluation.
+The easiest way of doing this is to add a rule like this to you reverse proxy configuration
+(this example is for Nginx):
+
+```py
+location ^~ /static/ {
+	alias /srv/opencast/opencast-dist-allinone/data/opencast/downloads/;
+	sendfile           on;
+}
+```
+
+Next, install a suitable import workflow in your target Opencast.
+You can use any workflow you want and even reprocess videos,
+but if you just want to import and publish media as they were in the old system,
+you may want to use the workflow [import.yaml](import.yaml).
+
+Update the credentials and the workflow in the `muigrate.py` to configure your Opencast source and target systems.
+
+Finally, start the migration:
+
+```
+❯ python migrate.py
+Importing ID-wiki-commons
+Create series response: 201
+…
+Importing e85fcb07-6943-4d09-b739-60daa756d769
+Ingest response: 200
+…
+```

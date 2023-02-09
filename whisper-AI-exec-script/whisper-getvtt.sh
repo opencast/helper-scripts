f6a4ccf4ea1052c8cf1e585c959ef412767c8bbf
Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>
2023-02-09 16:22:08 +0100

    Fixes #64
    Signed-off-by: Stefanos Georgopoulos <stefanos.georgopoulos@fau.de>

--- whisper-AI-exec-script/whisper-getvtt.sh
+++ whisper-AI-exec-script/whisper-getvtt.sh
@@ -12,12 +12,12 @@ ffmpeg -y -nostdin -nostats -i "$videoFile" -vn -acodec copy "/tmp/$eventId.aac"
 if [[ "$translate" == "translate" ]]
 then
     echo "Translating into english"
-    curl --max-time 7200 --location --request POST "$whisperServer/get-vtt?task=translate" \
+    curl --max-time 7200 --location --request POST "$whisperServer/asr?task=translate&output=vtt" \
     --form 'audio_file=@"/tmp/'"$eventId"'.aac"' -o "$outputVTT"
 else
     # Send audio for transcription
     echo "Transcribing audio"
-    curl --max-time 7200 --location --request POST "$whisperServer/get-vtt?task=transcribe" \
+    curl --max-time 7200 --location --request POST "$whisperServer/asr?task=transcribe&output=vtt" \
     --form 'audio_file=@"/tmp/'"$eventId"'.aac"' -o "$outputVTT"
 fi
 

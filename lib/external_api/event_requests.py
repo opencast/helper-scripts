a4a3b7ef9a463291d026c72baf45f9866c423f71
Katrin Ihler <ihler@elan-ev.de>
2023-09-18 18:28:50 +0200

    Extend External API event requests

--- lib/external_api/event_requests.py
+++ lib/external_api/event_requests.py
@@ -1,5 +1,6 @@
 from rest_requests.get_response_content import get_json_content
-from rest_requests.request import get_request
+from rest_requests.request import get_request, delete_request, NOT_FOUND
+from rest_requests.request_error import RequestError
 
 
 def get_events_of_series(base_url, digest_login, series_id):
@@ -38,3 +39,60 @@ def get_failed_events(base_url, digest_login):
     url = '{}/api/events/?filter=status:EVENTS.EVENTS.STATUS.PROCESSING_FAILURE'.format(base_url)
     response = get_request(url, digest_login, "failed events")
     return get_json_content(response)
+
+
+def get_event(base_url, digest_login, event_id):
+    """
+    Get event.
+
+    :param base_url: The URL for the request
+    :type base_url: str
+    :param digest_login: The login credentials for digest authentication
+    :type digest_login: DigestLogin
+    :param event_id: The id of the event to delete
+    :type event_id: str
+    :raise RequestError:
+    """
+    url = '{}/api/events/{}'.format(base_url, event_id)
+    response = get_request(url, digest_login, "/api/events/<id>")
+    return get_json_content(response)
+
+
+def event_exists(base_url, digest_login, event_id):
+    """
+    Check if an event exists.
+
+    :param base_url: The URL for the request
+    :type base_url: str
+    :param digest_login: The login credentials for digest authentication
+    :type digest_login: DigestLogin
+    :param event_id: The id of the event
+    :type event_id: str
+    :return: true if it exists, false otherwise
+    :rtype: bool
+    :raise RequestError:
+    """
+
+    try:
+        get_event(base_url, digest_login, event_id)
+    except RequestError as e:
+        if e.has_status_code() and e.get_status_code() == NOT_FOUND:
+            return False
+        raise e
+    return True
+
+
+def delete_event(base_url, digest_login, event_id):
+    """
+    Delete event.
+
+    :param base_url: The URL for the request
+    :type base_url: str
+    :param digest_login: The login credentials for digest authentication
+    :type digest_login: DigestLogin
+    :param event_id: The id of the event to delete
+    :type event_id: str
+    :raise RequestError:
+    """
+    url = '{}/api/events/{}'.format(base_url, event_id)
+    delete_request(url, digest_login, "/api/events/<id>")

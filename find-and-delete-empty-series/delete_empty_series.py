8b095ce731e881b9c52b3e1fde971af05e0c455b
Katrin Ihler <ihler@elan-ev.de>
2023-09-21 19:53:12 +0200

    Minor Fixes

--- find-and-delete-empty-series/delete_empty_series.py
+++ find-and-delete-empty-series/delete_empty_series.py
@@ -1,18 +1,20 @@
 import os
 import sys
+import time
+
+import config
 
 sys.path.append(os.path.join(os.path.abspath('..'), "lib"))
 
+from args.digest_login import DigestLogin
 from input_output.read_file import read_list_from_file, write_list_to_file
 from rest_requests.request_error import RequestError
-import config
-from args.digest_login import DigestLogin
 from rest_requests.series_requests import delete_series, series_has_events
 
 
 def delete_empty_series(admin_url, digest_login, empty_series):
     deleted_series = []
-    for series_id in empty_series:
+    for i, series_id in enumerate(empty_series):
         try:
             if series_has_events(admin_url, digest_login, series_id):
                 print("Series {} has events, not deleting!".format(series_id))
@@ -20,7 +22,10 @@ def delete_empty_series(admin_url, digest_login, empty_series):
             delete_series(admin_url, digest_login, series_id)
             deleted_series.append(series_id)
         except RequestError as e:
-            print("Series {} could not be deleted: {}", series_id, e)
+            print("Series {} could not be deleted: {}".format(series_id, e))
+        if (i + 1) % 100 == 0:
+            print("Checked {} series.".format(i + 1))
+            time.sleep(1)
     return deleted_series
 
 
